ed8226be58dba59c4e3a6fd0b7fb36ac59e87c94
guseva-sofya <guseva.sofya.p@gmail.com>
2023-06-04 12:32:48 +0200

    enumerate airports

--- flight_route_planner/flights.py
+++ flight_route_planner/flights.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
+from flight_route_planner import graphs
 
 AirportCode = str
 
@@ -26,4 +27,21 @@ class Route:
 def find_fastest_flight_route(
     airports: List[Airport], scheduled_flights: List[Flight]
 ) -> Route:
+    num_airports = len(airports)
+    graph: graphs.Graph = graphs.Graph(num_vertices=num_airports)
+
+    # for flight in scheduled_flights:
+    #     graph.add_edge(vertex1=, vertex2=1, weight=7)
+
     return Route([], 0)
+
+
+def enumerate_airports(airports: List[Airport]) -> Dict[AirportCode, graphs.Vertex]:
+    airports_to_vertices: Dict[AirportCode, graphs.Vertex] = {}
+    airport_codes = [airport.code for airport in airports]
+    # remove duplicates from a list: list -> set
+    sorted_unique_airport_codes = sorted(list(set(airport_codes)))
+
+    for index, airport_code in enumerate(sorted_unique_airport_codes):
+        airports_to_vertices[airport_code] = index
+    return airports_to_vertices
--- tests/test_flights.py
+++ tests/test_flights.py
@@ -19,3 +19,16 @@ def test_find_shortest_flight_route() -> None:
 
     assert ["LHR", "FRA", "CDG"] == fastest_route.airport_codes
     assert 2.5 == fastest_route.total_duration_in_hours
+
+
+def test_enumerate_airports() -> None:
+    airports = [
+        flights.Airport("LHR", "London Heathrow Airport"),
+        flights.Airport("CDG", "Paris Charles de Gaulle Airport"),
+        flights.Airport("FRA", "Frankfurt Airport"),
+        flights.Airport("FRA", "Frankfurt Airport"),
+    ]
+
+    airports_to_vertices = flights.enumerate_airports(airports)
+
+    assert {"CDG": 0, "FRA": 1, "LHR": 2} == airports_to_vertices
