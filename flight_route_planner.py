9b7fa827dfa1f4201b5e5c0bbd1cdd35886fc8ba
guseva-sofya <guseva.sofya.p@gmail.com>
2023-06-10 11:04:27 +0200

    load json data and refactor

--- flight_route_planner.py
+++ flight_route_planner.py
@@ -1,53 +1,50 @@
 import os
 import json
-from typing import List
-from flight_route_planner import flights
+from typing import Any, Callable, Dict, List
+from flight_route_planner import flight_routes
 
 
 def main():
     airports = load_airports()
-    loaded_flights = load_flights()
+    flights = load_flights()
     for airport in airports:
         print(airport)
 
-    for flight in loaded_flights:
+    for flight in flights:
         print(flight)
 
 
-def load_airports() -> List[flights.Airport]:
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    relative_airport_file_path = "flights_data/airports.json"
-    absolute_airport_file_path = os.path.join(
-        script_directory, relative_airport_file_path
-    )
-    with open(absolute_airport_file_path, "r") as file:
-        json_data = file.read()
-
+def load_airports() -> List[flight_routes.Airport]:
     def airport_hook(data):
-        return flights.Airport(data["code"], data["full_name"])
+        return flight_routes.Airport(data["code"], data["full_name"])
 
-    airports = json.loads(json_data, object_hook=airport_hook)
+    airports = load_json_data("flights_data/airports.json", airport_hook)
 
     return airports
 
 
-def load_flights() -> List[flights.Flight]:
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    relative_flight_file_path = "flights_data/flights.json"
-    absolute_flight_file_path = os.path.join(
-        script_directory, relative_flight_file_path
-    )
-    with open(absolute_flight_file_path, "r") as file:
-        json_data = file.read()
-
+def load_flights() -> List[flight_routes.Flight]:
     def flight_hook(data):
-        return flights.Flight(
+        return flight_routes.Flight(
             data["departure"], data["destination"], data["duration_in_hours"]
         )
 
-    loaded_flights = json.loads(json_data, object_hook=flight_hook)
+    flights = load_json_data("flights_data/flights.json", flight_hook)
 
-    return loaded_flights
+    return flights
+
+
+def load_json_data(
+    file_path: str, object_hook: Callable[[Dict[str, Any]], Any]
+) -> List[Any]:
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    absolute_file_path = os.path.join(script_directory, file_path)
+    with open(absolute_file_path, "r") as file:
+        json_data = file.read()
+
+    loaded_objects = json.loads(json_data, object_hook=object_hook)
+
+    return loaded_objects
 
 
 if __name__ == "__main__":
---
+++ tests/test_flight_routes.py
@@ -0,0 +1,38 @@
+import json
+from flight_route_planner import flight_routes
+
+
+def test_find_shortest_flight_route() -> None:
+    airports = [
+        flight_routes.Airport("LHR", "London Heathrow Airport"),
+        flight_routes.Airport("CDG", "Paris Charles de Gaulle Airport"),
+        flight_routes.Airport("FRA", "Frankfurt Airport"),
+        flight_routes.Airport("FCO", "Rome-Fiumicino Airport"),
+    ]
+
+    scheduled_flights = [
+        flight_routes.Flight("LHR", "CDG", 3),
+        flight_routes.Flight("LHR", "FRA", 2),
+        flight_routes.Flight("FRA", "CDG", 1),
+        flight_routes.Flight("FRA", "FCO", 1),
+        flight_routes.Flight("CDG", "FCO", 2),
+    ]
+
+    fastest_route = flight_routes.find_fastest_flight_route(
+        airports, scheduled_flights, "LHR", "FCO"
+    )
+
+    assert ["LHR", "FRA", "FCO"] == fastest_route.airport_codes
+    assert 3.0 == fastest_route.total_duration_in_hours
+
+
+def test_enumerate_airports() -> None:
+    airports = [
+        flight_routes.Airport("LHR", "London Heathrow Airport"),
+        flight_routes.Airport("CDG", "Paris Charles de Gaulle Airport"),
+        flight_routes.Airport("FRA", "Frankfurt Airport"),
+    ]
+
+    airports_to_vertices = flight_routes.enumerate_airports(airports)
+
+    assert {"CDG": 0, "FRA": 1, "LHR": 2} == airports_to_vertices
